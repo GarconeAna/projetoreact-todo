5fcdeb082a69b95e32231894d0f675cce44f633e
Ana Carolina <garcone.anaxx@gmail.com>
2021-10-20 01:35:22 -0300

    finalizando para o deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/shared/Header/Header';
+import Footer from './components/shared/Footer/Footer';
+import Home from './pages/Home/Home';
+import AddTodo from './pages/Add/Add';
+import GetTodo from './pages/GetTodo/GetTodo';
+import Delete from './pages/Delete/Delete';
+import Put from './pages/Put/Put';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Header/>
+        <Switch>
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/add" component={AddTodo}/>
+          <Route path="/view/:id" component={GetTodo}/>
+          <Route path="/edit/:id" component={Put}/>
+          <Route path="/delete/:id" component={Delete}/>
+        </Switch>
+      <Footer/>
+    </>
   );
 }
 
